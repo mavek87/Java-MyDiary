8f7f300a8c60384bb60b97f9a78799500b646b91
Matteo Veroni <>
2015-09-01 15:44:14 +0200

    All the screen's controllers are become ScreenManager Observers. When the screenManager changes screen (so when it changes) it notifies the change to all the screen controllers. Then when the change is notified to each controller each of they use they're update method to order to the view to RE-DRAW correctly.

--- src/com/matteoveroni/mydiary/screen/ScreenManager.java
+++ src/com/matteoveroni/mydiary/screen/ScreenManager.java
@@ -14,15 +14,15 @@ import javafx.stage.Stage;
  *
  * @author Matteo Veroni
  */
-public class ScreenManager implements Disposable, Observable{
+public class ScreenManager implements Disposable, Observable {
 
-    private final Map<ScreenType, Screen> applicationScreens = new HashMap<>();
+	private final Map<ScreenType, Screen> applicationScreens = new HashMap<>();
 	private final List<Observer> observersScreens = new ArrayList<>();
-    private Stage mainStage;
+	private Stage mainStage;
 
-    public ScreenManager(Stage applicationStage) {
-        this.mainStage = applicationStage;
-    }
+	public ScreenManager(Stage applicationStage) {
+		this.mainStage = applicationStage;
+	}
 
 	@Override
 	public void registerObserver(Observer newObserverScreen) {
@@ -36,48 +36,45 @@ public class ScreenManager implements Disposable, Observable{
 
 	@Override
 	public void notifyObservers() {
-		for(Observer observer : observersScreens){
+		for (Observer observer : observersScreens) {
 			observer.update();
 		}
 	}
 
-    public void setApplicationStage(Stage stage) {
-        this.mainStage = stage;
-    }
+	public void setApplicationStage(Stage stage) {
+		this.mainStage = stage;
+	}
 
-    public Stage getApplicationStage() {
-        return mainStage;
-    }
+	public Stage getApplicationStage() {
+		return mainStage;
+	}
 
-    public void loadScreen(Screen screen) {
-        try {
+	public void loadScreen(Screen screen) {
+		try {
 			ManageableScreen screenController = screen.getController();
 			screenController.setScreenManager(this);
-            applicationScreens.put(screen.getScreenType(), screen);
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
-    }
-	
-    public void useScreen(ScreenType screenTypeToUse) {
-        if (applicationScreens.containsKey(screenTypeToUse) && this.getApplicationStage() != null) {
-            
-            Screen screen = applicationScreens.get(screenTypeToUse);
-//            ManageableScreen screenController = screen.getController();
-//            screenController.updateScreen();
-			notifyObservers();
-            
-			// da rivedere le righe seguenti non so se servono!
-            mainStage.setScene(screen.getScene());
-            mainStage.show();
-        } else {
-            throw new RuntimeException("Screen Manager wasn\'t initialized with a main stage");
-        }
-    }
+			applicationScreens.put(screen.getScreenType(), screen);
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
 
-    public Scene getCurrentScene() {
-        return mainStage.getScene();
-    }
+	public void useScreen(ScreenType screenTypeToUse) {
+		if (applicationScreens.containsKey(screenTypeToUse) && this.getApplicationStage() != null) {
+			Screen screen = applicationScreens.get(screenTypeToUse);
+//          ManageableScreen screenController = screen.getController();
+//          screenController.updateScreen();
+			notifyObservers();
+			mainStage.setScene(screen.getScene());
+			mainStage.show();
+		} else {
+			throw new RuntimeException("Screen Manager wasn\'t initialized with a main stage");
+		}
+	}
+
+	public Scene getCurrentScene() {
+		return mainStage.getScene();
+	}
 
 	@Override
 	public void dispose() {
