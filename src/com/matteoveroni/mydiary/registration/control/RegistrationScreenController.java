42d31c37f3d91eb3e4a9c9797635c5dac3bba850
Matteo Veroni <matver87@gmail.com>
2015-11-01 14:00:09 +0100

    registration screen translated in english and italian

--- src/com/matteoveroni/mydiary/registration/control/RegistrationScreenController.java
+++ src/com/matteoveroni/mydiary/registration/control/RegistrationScreenController.java
@@ -31,157 +31,159 @@ import javax.swing.JOptionPane;
  */
 public class RegistrationScreenController implements Manageable, Initializable, Listener {
 
-	private Manager manager;
-	private final RegistrationModel model = new RegistrationModel();
-	private final UserData userToRegister = new UserData();
+    private Manager manager;
+    private final RegistrationModel model = new RegistrationModel();
+    private final UserData userToRegister = new UserData();
+    private ResourceBundle resourceBundle;
 
-	private final int MIN_USERNAME_LENGTH = 6;
-	private final int MIN_PASSWORD_LENGTH = 6;
+    private final int MIN_USERNAME_LENGTH = 6;
+    private final int MIN_PASSWORD_LENGTH = 6;
 
-	@FXML
-	private Label lbl_Username;
-	@FXML
-	private TextField txt_Username;
-	@FXML
-	private Label lbl_Password;
-	@FXML
-	private PasswordField psw_Password;
-	@FXML
-	private Label lbl_Name;
-	@FXML
-	private TextField txt_Name;
-	@FXML
-	private Label lbl_Surname;
-	@FXML
-	private TextField txt_Surname;
-	@FXML
-	private Label lbl_Age;
-	@FXML
-	private TextField txt_Age;
-	@FXML
-	private Label lbl_Title;
-	@FXML
-	private Button btn_Cancel;
-	@FXML
-	private Button btn_Register;
-	@FXML
-	private MenuBar menu;
-	@FXML
-	private Menu menu_file;
-	@FXML
-	private MenuItem menu_settings;
-	@FXML
-	private MenuItem menu_close;
-	@FXML
-	private Menu menu_help;
-	@FXML
-	private MenuItem menu_about;
+    @FXML
+    private Label lbl_Username;
+    @FXML
+    private TextField txt_Username;
+    @FXML
+    private Label lbl_Password;
+    @FXML
+    private PasswordField psw_Password;
+    @FXML
+    private Label lbl_Name;
+    @FXML
+    private TextField txt_Name;
+    @FXML
+    private Label lbl_Surname;
+    @FXML
+    private TextField txt_Surname;
+    @FXML
+    private Label lbl_Age;
+    @FXML
+    private TextField txt_Age;
+    @FXML
+    private Label lbl_Title;
+    @FXML
+    private Button btn_Cancel;
+    @FXML
+    private Button btn_Register;
+    @FXML
+    private MenuBar menu;
+    @FXML
+    private Menu menu_file;
+    @FXML
+    private MenuItem menu_settings;
+    @FXML
+    private MenuItem menu_close;
+    @FXML
+    private Menu menu_help;
+    @FXML
+    private MenuItem menu_about;
 
-	/**
-	 * Initializes the controller class.
-	 *
-	 * @param url
-	 * @param rb
-	 */
-	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-	}
+    /**
+     * Initializes the controller class.
+     *
+     * @param url
+     * @param resourceBundle
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.resourceBundle = resourceBundle;
+    }
 
-	@Override
-	public void update(DataObjectMessage pushedData) {
-		if (manager != null && manager.getLoggedInUser() != null) {
-			clearAllTheFieldsOnTheForm();
-		}
-	}
+    @Override
+    public void update(DataObjectMessage pushedData) {
+        if (manager != null && manager.getLoggedInUser() != null) {
+            clearAllTheFieldsOnTheForm();
+        }
+    }
 
-	@Override
-	public void setManager(Manager manager) {
-		this.manager = manager;
-		manager.registerListener(this);
-	}
+    @Override
+    public void setManager(Manager manager) {
+        this.manager = manager;
+        manager.registerListener(this);
+    }
 
-	@FXML
-	void cancelRegistration(ActionEvent event) {
-		manager.changeScreen(ScreensFramework.LOGIN_SCREEN);
-	}
+    @FXML
+    void cancelRegistration(ActionEvent event) {
+        manager.changeScreen(ScreensFramework.LOGIN_SCREEN);
+    }
 
-	@FXML
-	void registerUser(ActionEvent event) {
-		if (isUsernameAndPasswordInserted()) {
-			setNewUserInstanceUsingTheDataInsertedOnTheForm();
-			if (model.createNewUser(userToRegister)) {
-				JOptionPane.showMessageDialog(null,
-					"\n Username: " + userToRegister.getUsername()
-					+ "\n FirstName: " + userToRegister.getFirstName()
-					+ "\n LastName: " + userToRegister.getLastName()
-					+ "\n Age: " + userToRegister.getAge(), "User registered! ", JOptionPane.INFORMATION_MESSAGE);
-				manager.changeScreen(ScreensFramework.LOGIN_SCREEN);
-			} else {
-				clearUserFieldOnTheForm();
-				JOptionPane.showMessageDialog(null, "Username already taken by another user... please choose another one!", manager.getApplicationTitle() + "Username already exists", JOptionPane.ERROR_MESSAGE);
-			}
-		} else {
-			clearUserAndPasswordFieldsOnTheForm();
-			JOptionPane.showMessageDialog(null, "Username and Password can\'t be less than 6 characters long. Choose longer Username and Password!", manager.getApplicationTitle() + "Username/Password Invalid", JOptionPane.ERROR_MESSAGE);
-		}
-	}
+    @FXML
+    void registerUser(ActionEvent event) {
+        if (isUsernameAndPasswordInserted()) {
+            setNewUserInstanceUsingTheDataInsertedOnTheForm();
+            if (model.createNewUser(userToRegister)) {
+                JOptionPane.showMessageDialog(null,
+                    "\n " + resourceBundle.getString("username") + ": " + userToRegister.getUsername()
+                    + "\n " + resourceBundle.getString("firstname") + ": " + userToRegister.getFirstName()
+                    + "\n " + resourceBundle.getString("lastname") + ": " + userToRegister.getLastName()
+                    + "\n " + resourceBundle.getString("age") + ": " + userToRegister.getAge(), resourceBundle.getString("userRegistered"), JOptionPane.INFORMATION_MESSAGE);
+                manager.changeScreen(ScreensFramework.LOGIN_SCREEN);
+            } else {
+                clearUserFieldOnTheForm();
+                JOptionPane.showMessageDialog(null, resourceBundle.getString("usernameAlreadyTaken"), manager.getApplicationTitle() + resourceBundle.getString("usernameAlreadyExists"), JOptionPane.ERROR_MESSAGE);
+            }
+        } else {
+            clearUserAndPasswordFieldsOnTheForm();
+            JOptionPane.showMessageDialog(null, resourceBundle.getString("usernameAndPasswordShort"), manager.getApplicationTitle() + resourceBundle.getString("usernameOrPasswordInvalid"), JOptionPane.ERROR_MESSAGE);
+        }
+    }
 
-	@FXML
-	void menuSettingsClicked(ActionEvent event) {
-		manager.changeScreen(ScreensFramework.SETTINGS_SCREEN);
-	}
+    @FXML
+    void menuSettingsClicked(ActionEvent event) {
+        manager.changeScreen(ScreensFramework.SETTINGS_SCREEN);
+    }
 
-	@FXML
-	void menuCloseClicked(ActionEvent event) {
-		Command closeCommand = new MenuCloseCommand(manager);
-		closeCommand.execute();
-	}
+    @FXML
+    void menuCloseClicked(ActionEvent event) {
+        Command closeCommand = new MenuCloseCommand(manager);
+        closeCommand.execute();
+    }
 
-	@FXML
-	void menuAboutClicked(ActionEvent event) {
-		Command aboutCommand = new MenuAboutCommand(manager);
-		aboutCommand.execute();
-	}
+    @FXML
+    void menuAboutClicked(ActionEvent event) {
+        Command aboutCommand = new MenuAboutCommand(manager);
+        aboutCommand.execute();
+    }
 
-	private boolean isUsernameAndPasswordInserted() {
-		String insertedUsername = txt_Username.getText();
-		String insertedPassword = psw_Password.getText();
-		return (isUsernameValid(insertedUsername) && isPasswordValid(insertedPassword));
-	}
+    private boolean isUsernameAndPasswordInserted() {
+        String insertedUsername = txt_Username.getText();
+        String insertedPassword = psw_Password.getText();
+        return (isUsernameValid(insertedUsername) && isPasswordValid(insertedPassword));
+    }
 
-	private boolean isUsernameValid(String username) {
-		return (!username.trim().equals("") && username.length() >= MIN_USERNAME_LENGTH);
-	}
+    private boolean isUsernameValid(String username) {
+        return (!username.trim().equals("") && username.length() >= MIN_USERNAME_LENGTH);
+    }
 
-	private boolean isPasswordValid(String password) {
-		return (!password.trim().equals("") && password.length() >= MIN_PASSWORD_LENGTH);
-	}
+    private boolean isPasswordValid(String password) {
+        return (!password.trim().equals("") && password.length() >= MIN_PASSWORD_LENGTH);
+    }
 
-	private void setNewUserInstanceUsingTheDataInsertedOnTheForm() {
-		userToRegister.setUsername(txt_Username.getText());
-		userToRegister.setPassword(psw_Password.getText());
-		userToRegister.setFirstName(txt_Name.getText());
-		userToRegister.setLastName(txt_Surname.getText());
-		try {
-			userToRegister.setAge(Integer.parseInt(txt_Age.getText()));
-		} catch (NumberFormatException ex) {
-		}
-	}
+    private void setNewUserInstanceUsingTheDataInsertedOnTheForm() {
+        userToRegister.setUsername(txt_Username.getText());
+        userToRegister.setPassword(psw_Password.getText());
+        userToRegister.setFirstName(txt_Name.getText());
+        userToRegister.setLastName(txt_Surname.getText());
+        try {
+            userToRegister.setAge(Integer.parseInt(txt_Age.getText()));
+        } catch (NumberFormatException ex) {
+        }
+    }
 
-	private void clearUserAndPasswordFieldsOnTheForm() {
-		txt_Username.setText("");
-		psw_Password.setText("");
-	}
+    private void clearUserAndPasswordFieldsOnTheForm() {
+        txt_Username.setText("");
+        psw_Password.setText("");
+    }
 
-	private void clearUserFieldOnTheForm() {
-		txt_Username.setText("");
-	}
+    private void clearUserFieldOnTheForm() {
+        txt_Username.setText("");
+    }
 
-	private void clearAllTheFieldsOnTheForm() {
-		txt_Username.setText("");
-		psw_Password.setText("");
-		txt_Name.setText("");
-		txt_Surname.setText("");
-		txt_Age.setText("");
-	}
+    private void clearAllTheFieldsOnTheForm() {
+        txt_Username.setText("");
+        psw_Password.setText("");
+        txt_Name.setText("");
+        txt_Surname.setText("");
+        txt_Age.setText("");
+    }
 }
